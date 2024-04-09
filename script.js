056e7fc9d7e9c882bdf6bbd3c6a24dc030ed990c
nikita647 <jnikita647@gmail.com>
2024-04-09 15:54:21 +0530

    Your commit message here

---
+++ script.js
@@ -0,0 +1,24 @@
+const countValue=document.querySelector('#counter');
+const increment=()=>{
+    //get the value from ui
+let value=parseInt(countValue.innerText);
+//update the value
+value=value+1;
+//set the value onto ui
+countValue.innerText=value;
+};
+
+
+const decrement=()=>{
+  //get the value from ui
+  let value=parseInt(countValue.innerText);
+  
+  //update the value
+  value=value-1;
+  //set the value onto ui
+  countValue.innerText=value;
+}
+
+
+
+
